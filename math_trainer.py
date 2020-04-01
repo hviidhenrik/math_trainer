664126593e895fc1235fbd54b2f6bb62cfb40c74
hviidhenrik <hviidhenrik@outlook.com>
2020-04-02 00:12:03 +0200

    added saving functionality and other stuff

--- math_trainer.py
+++ math_trainer.py
@@ -12,6 +12,7 @@ TO DO:
     - implement different difficulty levels
     - add exception to handle empty input as in if user just pushes enter 
        and nothing else 
+    - make quick start mandatory if directory is empty
 """
 
 
@@ -25,8 +26,8 @@ class Problem:
         self.num2 = num2
         self.operator = operator
         self.result = operator(num1, num2)
+        self.answer = np.nan
         self.time = np.nan
-        # self.id = "{}{}".format(num1, num2) # not implemented yet
         
     def __str__(self):
         op = "+" if self.operator == np.add else "-" 
@@ -36,76 +37,119 @@ class Problem:
         op = "+" if self.operator == np.add else "-" 
         return "{} {} {}".format(self.num1, op, self.num2)
     
+    def __del__(self):
+        Problem.instance_count -= 1
+    
+#%% 
+import os
+quick_start = input("Quick start or use existing file? [quick/existing]\n")
+quick_start = quick_start.lower().startswith("q")
+
+
+if not quick_start:
+    # print an overview of already existing csv files in directory
+    print("The following data files were found already:")
+    files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith(".csv")]
+    print(files)
+    file = input("Write the name of the file you wish to use:\n")
+    file = file if file.endswith(".csv") else file + ".csv"
+
+
 #%% testing
 from timeit import default_timer as timer
 
-# print an overview of alread existing csv files in directory
 
-# ask which one to save to, or make a new one
-    # if existing one chosen, append results
-    # else make new file
-
+# init counts and lists
 count = 0
 corrects = 0
 prob_array = []
-time_array = []
-answer_array = []
 int_min = input("Lowest possible integer: ")
 int_max = input("Highest possible integer: ")
+
+# run the main game/practice loop
 while(1):
     prob = Problem(np.random.randint(int_min,int_max), 
                    np.random.randint(int_min,int_max), 
                    np.add)
     start = timer()
     input_answer = input("{} = ".format(prob))
+    
+    # if the user wishes to stop the game
     if input_answer.lower() == "stop":
         end = timer()
+        del(prob)
         break
+    
+    # if provided answer is correct
     elif prob.result == int(input_answer):
         print("Correct")
         corrects += 1
+    # if it's not correct
     else:
         print("Incorrect")
+        
+    # store timing, problem and answer and increment problem count
     end = timer()
-    prob.time = end - start
-    time_array.append(end-start)
+    prob.time = end-start
     count += 1
     prob_array.append(prob)
-    answer_array.append(int(input_answer))
+    prob.answer = int(input_answer)
 
+# compute mean response time
+mean_time = np.mean([prob.time for prob in prob_array])
 
+# print some stats
 print("{}% correct ({} out of {})".format(np.round(100*corrects/count,2), corrects, count))
-print("Average repsonse time: ", np.round(np.mean(time_array),2), "seconds")
+print("Average response time: ", np.round(np.mean(mean_time),2), "seconds")
 
 #%% turn into a dataset
 import pandas as pd
 
-df = pd.DataFrame({"time": time_array,
+df = pd.DataFrame({"time":   [prob.time for prob in prob_array],
                    "num1":   [prob.num1 for prob in prob_array],
                    "num2":   [prob.num2 for prob in prob_array],
                    "result": [prob.result for prob in prob_array],
-                   "answer": answer_array})
+                   "answer": [prob.answer for prob in prob_array]})
 df["correct"] = [1 if diff == 0 else 0 for diff in df.answer - df.result] # codes it as binary integers
 
-df.to_csv("math_practice.csv", sep="\t",)
+#%% writing or appending to a file
 
-#%% analysis
+if quick_start:
+    print("----------------------------------------")
+    print("The following files were already found in the directory:")
+    print(files)
+    save_file = input("If you wish to save to an existing file seen above, simply type its name - otherwise, a new file will be created: \n")
+    save_file = save_file if save_file.endswith(".csv") else save_file + ".csv"
+    mode = "a" if save_file in files else "w" # append or write
+    df.to_csv(save_file, index = False, mode = mode, header = mode == "w")
+else:
+    df.to_csv(file, index = False, mode = "a", header = False)
+
+df1 = pd.read_csv("math_practice.csv")
+# df1
+# df.to_csv("math_practice.csv",index=False)
+
+print(df1)
+
+#%% statistical analysis
 from sklearn import linear_model
 
 df.columns
-X = df[["num1","num2","result","answer","correct"]]
-X = df[["correct"]]
+df.iloc[[1],5] = 0
+
+X = df[["num1","num2","result","correct"]]
+# X = df[["correct"]]
 y = df["time"]
 
-regr = linear_model.LinearRegression()
-regr.fit(X,y)
-regr.coef_
+lm = linear_model.LinearRegression()
+lm.fit(X,y)
+lm.coef_
+
+
 
 
 #%% own model analysis
 from scipy import stats
-lm = LinearRegression()
-lm.fit(X,y)
 params = np.append(lm.intercept_,lm.coef_)
 predictions = lm.predict(X)
 
@@ -119,7 +163,6 @@ MSE = (sum((y-predictions)**2))/(len(newX)-len(newX.columns))
 var_b = MSE*(np.linalg.inv(np.dot(newX.T,newX)).diagonal())
 sd_b = np.sqrt(var_b)
 ts_b = params/ sd_b
-
 p_values =[2*(1-stats.t.cdf(np.abs(i),(len(newX)-1))) for i in ts_b]
 
 sd_b = np.round(sd_b,3)
