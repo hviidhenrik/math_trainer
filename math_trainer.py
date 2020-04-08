37dfd8d8e2921ce4117920ef34f4726211724c5f
Henrik Hviid Hansen <hviidhenrik@outlook.com>
2020-04-09 01:18:42 +0200

    a lot of updated functionality e.g. error checking in user input, choice of save-file by number and more

--- math_trainer.py
+++ math_trainer.py
@@ -11,13 +11,15 @@ TO DO:
     - add more math operations
     - implement different difficulty levels
     - add exception to handle empty input as in if user just pushes enter 
-       and nothing else 
+       and nothing else
+    - add exception for bad input in all prompts
     - make quick start mandatory if directory is empty
+    - fix divisionbyzero error caused by "stop" on first problem posed
 """
-
 
 import numpy as np
 
+# define a problem class
 class Problem:
     instance_count = 0
     def __init__(self, num1, num2, operator):
@@ -39,68 +41,134 @@ class Problem:
     
     def __del__(self):
         Problem.instance_count -= 1
+        
+# helper function to take validate and take integer input from the user   
+def input_number(message,exception_message = None):
+    # if no exception message is provided, use a standard one
+    if exception_message is None:
+            exception_message = "Bad input detected. Please provide an integer"
+    # loop to check for and obtain proper integer input
+    while True:
+        user_input = input(message)
+        try:
+            user_input = int(user_input)
+        except ValueError:
+            print(exception_message)
+            continue
+        else:
+            break
+    return user_input
     
-#%% 
+#%% setup for saving to a file or not
+        
 import os
-quick_start = input("Quick start or use existing file? [quick/existing]\n")
-quick_start = quick_start.lower().startswith("q")
 
+# prompt the user whether to quick start or use existing/new file and check input
+while True:
+    start = input("Quick start (q) or save results to existing/new file (e)?\n")
+    # check the input for validity
+    try:
+        start = start[0].lower()
+    except IndexError:    
+        print("Bad input, please provide wether to use quick start or new/existing file [q/e]\n")
+        continue
+    else:
+        if start in ["q","e"]:
+            break
 
-if not quick_start:
+# if existing/new file was chosen, let user decide the file to save to
+if start == "e":
     # print an overview of already existing csv files in directory
-    print("The following data files were found already:")
+    print("The following data files were found already:\n")
     files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith(".csv")]
-    print(files)
-    file = input("Write the name of the file you wish to use:\n")
-    file = file if file.endswith(".csv") else file + ".csv"
+    [print(idx+1,f) for idx, f in enumerate(files)]
 
+    # prompt the user for a desired file
+    file = input("Write the name or number of the file you wish to use:\n")
+    
+    # check the given input and convert to integer if a number was given
+    try:
+        file = int(file)
+    # exception raised if it is a string input
+    except ValueError:
+        file = file if file.endswith(".csv") else file + ".csv"
+        # check if the file exists to determine the inclusion of a header in csv
+        file_exists = file in files 
+    # if integer input
+    else:
+        idx = file - 1
+        file = files[idx]
+        file_exists = True
 
-#%% testing
+#%% main section that runs the practice loop
 from timeit import default_timer as timer
 
 
-# init counts and lists
+# initiate counts, lists and integer limits
 count = 0
 corrects = 0
 prob_array = []
-int_min = input("Lowest possible integer: ")
-int_max = input("Highest possible integer: ")
 
-# run the main game/practice loop
-while(1):
+# get upper and lower limits of the math problems to be posed
+while True:
+    int_min = input("Lowest possible integer: ")
+    int_max = input("Highest possible integer: ")
+    
+    # check that the provided limits are actually integers, else prompt for it again
+    try:
+        int_min, int_max = [int(limit) for limit in [int_min,int_max]]
+    except ValueError:
+        print("Bad input detected. The limits must be integer numbers")
+        continue
+    else:
+        break
+
+
+# run the problem generating loop
+while True:
+    # initiate a problem instance
     prob = Problem(np.random.randint(int_min,int_max), 
                    np.random.randint(int_min,int_max), 
                    np.add)
-    start = timer()
-    input_answer = input("{} = ".format(prob))
+    timer_start = timer()
     
-    # if the user wishes to stop the game
-    if input_answer.lower() == "stop":
-        end = timer()
+    # the answer from the user
+    input_answer = input("{} = ".format(prob))
+        
+    # check the input - if 's' is detected, stop the loop
+    if input_answer.lower().startswith("s"):
+        timer_end = timer()
         del(prob)
         break
-    
-    # if provided answer is correct
-    elif prob.result == int(input_answer):
-        print("Correct")
-        corrects += 1
-    # if it's not correct
+    # else try to convert the input to integer
     else:
-        print("Incorrect")
+        # try to convert given answer to integer else print message and pose new problem
+        try:
+            input_answer = int(input_answer)
+        except ValueError:
+            print("Bad input detected - please provide integer numbers or \"stop\" (s)")
+            continue       
+        
+        # if provided answer is correct
+        if prob.result == input_answer:
+            print("Correct")
+            corrects += 1
+        # if it's not correct
+        else:
+            print("Incorrect")
         
     # store timing, problem and answer and increment problem count
-    end = timer()
-    prob.time = end-start
+    timer_end = timer()
+    prob.time = timer_end-timer_start
     count += 1
     prob_array.append(prob)
-    prob.answer = int(input_answer)
+    prob.answer = input_answer
 
-# compute mean response time
-mean_time = np.mean([prob.time for prob in prob_array])
-
-# print some stats
-print("{}% correct ({} out of {})".format(np.round(100*corrects/count,2), corrects, count))
-print("Average response time: ", np.round(np.mean(mean_time),2), "seconds")
+# compute and print mean response time
+if len(prob_array) > 0:
+    mean_time = np.mean([prob.time for prob in prob_array])
+    print("{}% correct ({} out of {})".format(np.round(100*corrects/count,2), corrects, count))
+    print("Average response time: ", np.round(np.mean(mean_time),2), "seconds")
 
 #%% turn into a dataset
 import pandas as pd
@@ -114,62 +182,43 @@ df["correct"] = [1 if diff == 0 else 0 for diff in df.answer - df.result] # code
 
 #%% writing or appending to a file
 
-if quick_start:
-    print("----------------------------------------")
-    print("The following files were already found in the directory:")
-    print(files)
-    save_file = input("If you wish to save to an existing file seen above, simply type its name - otherwise, a new file will be created: \n")
-    save_file = save_file if save_file.endswith(".csv") else save_file + ".csv"
-    mode = "a" if save_file in files else "w" # append or write
-    df.to_csv(save_file, index = False, mode = mode, header = mode == "w")
-else:
-    df.to_csv(file, index = False, mode = "a", header = False)
+# if quick start was chosen in the beginning prompt for saving
+if start == "q" and len(prob_array) > 0:
+    # ask if user wishes to save results to a file
+    input_savefile = input("Save your results to a file? [y/n]\n")
+    
+    # if yes, print list of already existing csv files in directory to save to
+    if input_savefile.lower().startswith("y"):
+        # print an overview of already existing csv files in directory
+        print("----------------------------------------")
+        print("The following data files were found already:\n")
+        files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith(".csv")]
+        [print(idx+1,f) for idx, f in enumerate(files)]
+    
+        # prompt the user for a desired file
+        file = input("Write the name or number of the file you wish to use:\n")
+        
+        # check the given input and convert to integer if a number was given
+        try:
+            file = int(file)
+        # exception raised if it is a string input
+        except ValueError:
+            file = file if file.endswith(".csv") else file + ".csv"
+        # if integer input
+        else:
+            idx = file - 1
+            file = files[idx]
+        
+        mode = "a" if file in files else "w" # append or write
+        df.to_csv(file, index = False, mode = mode, header = mode == "w")
 
-df1 = pd.read_csv("math_practice.csv")
-# df1
-# df.to_csv("math_practice.csv",index=False)
+# else simply save to the file specified in the beginning
+elif start == "e" and len(prob_array) > 0:
+    df.to_csv(file, index = False, mode = "a", header = not file_exists)
+
+df1 = pd.read_csv(file)
+# # df1
+# # df.to_csv("math_practice.csv",index=False)
 
 print(df1)
 
-#%% statistical analysis
-from sklearn import linear_model
-
-df.columns
-df.iloc[[1],5] = 0
-
-X = df[["num1","num2","result","correct"]]
-# X = df[["correct"]]
-y = df["time"]
-
-lm = linear_model.LinearRegression()
-lm.fit(X,y)
-lm.coef_
-
-
-
-
-#%% own model analysis
-from scipy import stats
-params = np.append(lm.intercept_,lm.coef_)
-predictions = lm.predict(X)
-
-newX = pd.DataFrame({"Constant":np.ones(len(X))}).join(pd.DataFrame(X))
-MSE = (sum((y-predictions)**2))/(len(newX)-len(newX.columns))
-
-# Note if you don't want to use a DataFrame replace the two lines above with
-# newX = np.append(np.ones((len(X),1)), X, axis=1)
-# MSE = (sum((y-predictions)**2))/(len(newX)-len(newX[0]))
-
-var_b = MSE*(np.linalg.inv(np.dot(newX.T,newX)).diagonal())
-sd_b = np.sqrt(var_b)
-ts_b = params/ sd_b
-p_values =[2*(1-stats.t.cdf(np.abs(i),(len(newX)-1))) for i in ts_b]
-
-sd_b = np.round(sd_b,3)
-ts_b = np.round(ts_b,3)
-p_values = np.round(p_values,3)
-params = np.round(params,4)
-
-myDF3 = pd.DataFrame()
-myDF3["Coefficients"],myDF3["Standard Errors"],myDF3["t values"],myDF3["Probabilites"] = [params,sd_b,ts_b,p_values]
-print(myDF3)
