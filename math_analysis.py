4490dbe122a224e6efc452709bc488cbc66bb20c
Henrik Hviid Hansen <hviidhenrik@outlook.com>
2020-04-09 01:19:21 +0200

    moved the statistical analysis to a separate file

---
+++ math_analysis.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 22:03:52 2020
+
+@author: Henrik
+"""
+
+#%% statistical analysis
+from sklearn import linear_model
+
+df.columns
+df.iloc[[1],5] = 0
+
+X = df[["num1","num2","result","correct"]]
+# X = df[["correct"]]
+y = df["time"]
+
+lm = linear_model.LinearRegression()
+lm.fit(X,y)
+lm.coef_
+
+
+
+
+#%% own model analysis
+from scipy import stats
+params = np.append(lm.intercept_,lm.coef_)
+predictions = lm.predict(X)
+
+newX = pd.DataFrame({"Constant":np.ones(len(X))}).join(pd.DataFrame(X))
+MSE = (sum((y-predictions)**2))/(len(newX)-len(newX.columns))
+
+# Note if you don't want to use a DataFrame replace the two lines above with
+# newX = np.append(np.ones((len(X),1)), X, axis=1)
+# MSE = (sum((y-predictions)**2))/(len(newX)-len(newX[0]))
+
+var_b = MSE*(np.linalg.inv(np.dot(newX.T,newX)).diagonal())
+sd_b = np.sqrt(var_b)
+ts_b = params/ sd_b
+p_values =[2*(1-stats.t.cdf(np.abs(i),(len(newX)-1))) for i in ts_b]
+
+sd_b = np.round(sd_b,3)
+ts_b = np.round(ts_b,3)
+p_values = np.round(p_values,3)
+params = np.round(params,4)
+
+myDF3 = pd.DataFrame()
+myDF3["Coefficients"],myDF3["Standard Errors"],myDF3["t values"],myDF3["Probabilites"] = [params,sd_b,ts_b,p_values]
+print(myDF3)
