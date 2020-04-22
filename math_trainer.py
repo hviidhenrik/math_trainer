f5917a41bbfce915090badebfaaa8d62545b94d6
Henrik Hviid Hansen <hviidhenrik@outlook.com>
2020-04-22 21:28:46 +0200

    made more input error checking and better interface

--- math_trainer.py
+++ math_trainer.py
@@ -7,11 +7,17 @@ Created on Wed Mar 25 20:39:06 2020
 TO DO:
     - add option to cancel choice in input, e.g. if saving to a file, cancel it
         if "cancel" or "stop" is detected as file name
-    - improve integer division mode give actually non-trivial problems that arent 
+    - improve integer division mode to give actually non-trivial problems that arent 
         a/1 or a/a - prevent prime numbers
     - implement a true division mode where results can be rounded fractions, e.g. 0.33
-    - implement different/fixed difficulty levels
     - make graphical interface
+    - catch empty input when selecting file to save
+    - ensure that results can only be saved to files with the same game mode,
+       e.g. addition, multiplication etc) to avoid data getting mixed up -
+       alternatively, add a column in the data that shows the operation for
+       each problem/row
+    - add option to "go back" when selecting game mode, whether to save and such
+    - add hidden "quit option" to all input prompts
 """
 
 import numpy as np
@@ -30,9 +36,10 @@ class Problem:
         if self.operator == np.divide and self.num2 == 0:
             self.num2 += 1
         
-        self.result = np.round(operator(self.num1, self.num2),2)
-        self.answer = np.nan
-        self.time = np.nan
+        self.result = np.round(operator(self.num1, self.num2),2) # the correct result
+        self.answer = np.nan  # the user provided answer
+        self.error = np.nan   # how far off the provided answer is
+        self.time = np.nan    # how long it took to answer
         self.date = date.today().strftime("%b-%d-%Y")
         
     def __str__(self):
@@ -80,41 +87,70 @@ def input_number(message,exception_message = None):
 import os
 
 # prompt the user whether to quick start or use existing/new file and check input
-while True:
-    start = input("Quick start (q) or save results to existing/new file (e)?\n")
-    # check the input for validity
-    try:
-        start = start[0].lower()
-    except IndexError:    
-        print("Bad input, please provide wether to use quick start or new/existing file [q/e]\n")
-        continue
-    else:
-        if start in ["q","e"]:
-            break
 
-# if existing/new file was chosen, let user decide the file to save to
-if start == "e":
-    # print an overview of already existing csv files in directory
-    print("The following data files were found already:\n")
-    files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith(".csv")]
-    [print(idx+1,f) for idx, f in enumerate(files)]
-
-    # prompt the user for a desired file
-    file = input("Write the name or number of the file you wish to use:\n")
     
-    # check the given input and convert to integer if a number was given
+print("WELCOME TO MATH TRAINER v1.0")
+print("----------------------------")
+print("How would you like to play?\n")
+    
+    
+    # print("Quick start (q) or save results to existing/new file (e)?\n")
+    
+    # start = input("Quick start (q) or save results to existing/new file (e)?\n")
+while True:
+    print("[1] Quick start")
+    print("[2] Create/select a file to save to")
+    start = input("Your choice: ")
+    print("----------------------------\n")
+    
+    # check the input for validity
+    
+    ##
+    modes = {"1":"quick", "2":"savefile","q":"quit"}
+    print()
+    # validate input and prompt user again if erroneous input was detected
     try:
-        file = int(file)
-    # exception raised if it is a string input
-    except ValueError:
-        file = file if file.endswith(".csv") else file + ".csv"
-        # check if the file exists to determine the inclusion of a header in csv
-        file_exists = file in files 
-    # if integer input
+        start = modes[start]
+    except KeyError:
+        print("Bad input detected. Must be either 1 or 2\n")
+    # hidden quit option (mostly for my own testing purposes)
     else:
-        idx = file - 1
-        file = files[idx]
-        file_exists = True
+        if start == "quit":
+            raise SystemExit
+        else:
+            break
+            
+# if existing/new file was chosen, let user decide the file to save to
+if start == "savefile":
+        # print an overview of already existing csv files in directory
+    print("\nThe following data files were found already:\n")
+    while True:
+        files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith(".csv")]
+        [print("[", idx+1,"] ",f,sep="") for idx, f in enumerate(files)]
+    
+        # prompt the user for a desired file
+        file = input("Write the name or number of the file you wish to use:\n")
+        
+        # check the given input and convert to integer if a number was given
+        try:
+            file = int(file)
+        # exception raised if it is a string input
+        except ValueError:
+            # if input is empty ask user to provide a better filename
+            if all(i == " " for i in file):
+                print("\nEmpty input detected. Please provide a filename or number\n")
+                continue
+            # if filename isnt empty, check for or append .csv to it
+            file = file if file.endswith(".csv") else file + ".csv"
+            # check if the file exists to determine the inclusion of a header in csv
+            file_exists = file in files 
+            break
+        # if integer input
+        else:
+            idx = file - 1
+            file = files[idx]
+            file_exists = True
+            break
 
 #%% main section that runs the practice loop
 from timeit import default_timer as timer
@@ -127,14 +163,22 @@ prob_array = []
 
 # get desired math operation using input and a predefined dictionary of operations
 while True:
-    operator = input("Addition/subtraction, multiplication or division? [a/m/d]\n")
-    ops = {"a":np.add, "m":np.multiply, "d":np.divide}
-
+    print("[1] Addition/subtraction")
+    print("[2] Multiplication")
+    print("[3] Division")
+          
+    operator = input("Your choice: ")
+    print("----------------------------\n")
+    
+    # operator = input("Addition/subtraction, multiplication or division? [a/m/d]\n")
+    # ops = {"a":np.add, "m":np.multiply, "d":np.divide}
+    ops = {"1":np.add, "2":np.multiply, "3":np.divide}
+    
     # validate input and prompt user again if erroneous input was detected
     try:
         operator = ops[operator]
     except KeyError:
-        print("Bad input detected. Must be either \"a\",\"m\" or \"d\"")
+        print("Bad input detected. Must be either 1, 2 or 3 as below: \n")
         continue
     else:
         break
@@ -234,11 +278,19 @@ df = pd.DataFrame({"time":   [prob.time for prob in prob_array],
                    "answer": [prob.answer for prob in prob_array],
                    "date":   [prob.date for prob in prob_array]})
 df["correct"] = [1 if diff == 0 else 0 for diff in df.answer - df.result] # codes it as binary integers
+df["operation"] = operator
 
+#%% for adding new columns to existing data - NOT RUN! 
+# filename88 = "filename-goes-here.csv"
+# df88 = pd.read_csv(filename88)
+# df88["operation"] = np.multiply
+# df88.to_csv(filename88,index=False)
+# df99 = pd.read_csv(filename88)
+# df99
 #%% writing or appending to a file
 
 # if quick start was chosen in the beginning prompt for saving
-if start == "q" and len(prob_array) > 0:
+if start == "quick" and len(prob_array) > 0:
     # ask if user wishes to save results to a file
     input_savefile = input("Save your results to a file? [y/n]\n")
     
@@ -247,32 +299,73 @@ if start == "q" and len(prob_array) > 0:
         # print an overview of already existing csv files in directory
         print("----------------------------------------")
         print("The following data files were found already:\n")
-        files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith(".csv")]
-        [print(idx+1,f) for idx, f in enumerate(files)]
-    
-        # prompt the user for a desired file
-        file = input("Write the name or number of the file you wish to use:\n")
+        while True:
+            files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith(".csv")]
+            [print(idx+1,f) for idx, f in enumerate(files)]
         
-        # check the given input and convert to integer if a number was given
-        try:
-            file = int(file)
-        # exception raised if it is a string input
-        except ValueError:
-            file = file if file.endswith(".csv") else file + ".csv"
-        # if integer input
-        else:
-            idx = file - 1
-            file = files[idx]
-        
+            # prompt the user for a desired file
+            file = input("Write the name or number of the file you wish to use:\n")
+            # check the given input and convert to integer if a number was given
+            try:
+                file = int(file)
+            # exception raised if it is a string input
+            except ValueError:
+                # if input is empty ask user to provide a better filename
+                if all(i == " " for i in file):
+                    print("\nEmpty input detected. Please provide a filename or number\n")
+                    continue
+                file = file if file.endswith(".csv") else file + ".csv"
+                break
+            # if integer input
+            else:
+                idx = file - 1
+                file = files[idx]
+                break
+            
         mode = "a" if file in files else "w" # append or write
         df.to_csv(file, index = False, mode = mode, header = mode == "w")
 
 # else simply save to the file specified in the beginning
-elif start == "e" and len(prob_array) > 0:
+elif start == "savefile" and len(prob_array) > 0:
     df.to_csv(file, index = False, mode = "a", header = not file_exists)
 
 df1 = pd.read_csv(file)
 # # df1
 # # df.to_csv("math_practice.csv",index=False)
-
+print("")
 print(df1)
+
+
+#%% quick analysis
+from scipy import stats
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+dates = df1.date.unique()
+date_arrays = [df1[df1["date"] == date][["time","date"]] for date in dates]
+
+# print mean and sd
+df5 = pd.DataFrame([(np.mean(dates.time),
+                      np.std(dates.time),
+                      dates.date.unique()) for dates in date_arrays],
+                    columns=["mean","sd","date"])
+print(df5)
+
+# perform t-test with unequal variances
+first = date_arrays[0].time
+last = date_arrays[-1].time
+
+stats.ttest_ind(first,last,equal_var=False)
+
+
+# plot timing distributions
+sns.set(color_codes=True)
+sns.kdeplot(first,label="First")
+sns.kdeplot(last,label = "Latest")
+plt.legend()
+plt.title("Timing distributions")
+plt.xlabel("Seconds")
+plt.ylabel("Frequency")
+
+
+
